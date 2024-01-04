d9a913144bfadb158339728a58579e2fce63c95b
Lavie-Zanzuri <154608667+Lavie-Zanzuri@users.noreply.github.com>
2024-01-04 09:00:57 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -5,13 +5,13 @@
 public class InOrder {
 	public static void main (String[] args) {
 		//// Write your code here
-		int num1 = ((int)(Math.random()*10));
+		int num1 = ((int)(Math.random() * 10));
 		System.out.print(num1);
-		int num2 = ((int)(Math.random()*10));
+		int num2 = ((int)(Math.random() * 10));
 			while(num1 <= num2)	{
 				System.out.print(" " + num2);
 				num1 = num2;
-				num2 = ((int)(Math.random()*10));
+				num2 = ((int)(Math.random() * 10));
 			}
 	}
 }
