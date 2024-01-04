fe6cd4fbe613059980d1f1487e1fe2c42041c2e0
Lavie-Zanzuri <154608667+Lavie-Zanzuri@users.noreply.github.com>
2024-01-04 08:58:58 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,11 +5,11 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
 		int n = Integer.parseInt(args[0]);
-		for(int i=0;i<n;i++) {
-			if(i!=0) {
+		for(int i = 0;i < n;i ++) {
+			if(i != 0) {
 				System.out.println();
 			}				
-			for(int j=0; j<n;j++) {
+			for(int j = 0;j < n;j ++) {
 				if(i % 2 == 1) {
 					System.out.print(" *");
 				}
